c7f5f0a2320634af6e81e568cfd880e914e37448
Ekaterina Bruch <ekaterinabruch@gmail.com>
2023-07-20 12:48:13 +0200

    Outsource styledcomponents to separate file for List

--- components/RoomList/index.js
+++ components/RoomList/index.js
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import { RoomListContainer, RoomListTitle, RoomListItem } from "./styles";
 export default function RoomList() {
   // Dummy data for the rooms array
 
@@ -9,23 +9,6 @@ export default function RoomList() {
     { id: 4, name: "Room 404" },
     { id: 5, name: "Room 505" },
   ];
-
-  const RoomListContainer = styled.div`
-    text-align: center;
-  `;
-
-  const RoomListTitle = styled.h3`
-    color: #ff0054;
-  `;
-  const RoomListItem = styled.li`
-    list-style: none;
-    font-size: 20px;
-    margin-bottom: 40px;
-    background-color: #ced4da;
-    border-radius: 4px;
-    padding: 10px;
-    margin-right: 30px;
-  `;
 
   return (
     <RoomListContainer>

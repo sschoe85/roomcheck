46894cec956075318f547fd2e1a2fec85f4b4afe
Ekaterina Bruch <ekaterinabruch@gmail.com>
2023-07-21 17:16:16 +0200

    Implement toggle functionality (hydration error)

--- components/RoomButton/index.js
+++ components/RoomButton/index.js
@@ -1,5 +1,44 @@
+import { useState } from "react";
 import { StyledRoomButton } from "./styles";
+import { useEffect } from "react";
 
-export default function RoomButton({ room }) {
-  return <StyledRoomButton state={room.state}>{room.state}</StyledRoomButton>;
+export default function RoomButton({
+  room,
+  updateRoomState,
+  initialRoomState,
+}) {
+  //state to keep track of the current room state
+  const [currentState, setCurrentState] = useState(initialRoomState);
+
+  useEffect(() => {
+    setCurrentState(initialRoomState);
+  }, [initialRoomState]);
+
+  function toggleState() {
+    let newState;
+    switch (currentState) {
+      case "Keine Angabe":
+        newState = "Plätze frei";
+        break;
+      case "Plätze frei":
+        newState = "Wenige Plätze frei";
+        break;
+      case "Wenige Plätze frei":
+        newState = "Plätze belegt";
+        break;
+      case "Plätze belegt":
+        newState = "Keine Angabe";
+        break;
+      default:
+        newState = "Keine Angabe";
+    }
+    setCurrentState(newState);
+    updateRoomState(room.id, newState); //aktualisierter Zustand wird an RoomList übergeben
+  }
+
+  return (
+    <StyledRoomButton onClick={toggleState} state={currentState}>
+      {currentState}
+    </StyledRoomButton>
+  );
 }

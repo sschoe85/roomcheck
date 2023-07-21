2f7883243d660457f5f853daa158bc463d079fc7
Ekaterina Bruch <ekaterinabruch@gmail.com>
2023-07-21 13:11:00 +0200

    display the button color based on the corresponding status text

--- components/RoomButton/styles.js
+++ components/RoomButton/styles.js
@@ -7,12 +7,12 @@ const StyledRoomButton = styled.button`
   border: none;
   padding: 0 20px;
   height: 45px;
+  width: 200px;
   line-height: 45px;
   border-radius: 7px;
   font-weight: 400;
   font-size: 16px;
-  background: #d9d9d9;
-  color: #343a40;
+  color: #ffff;
   box-shadow: 0 4px 14px 0 rgb(0 0 0 / 10%);
   transition: background 0.2s ease, color 0.2s ease, box-shadow 0.2s ease;
 
@@ -20,6 +20,16 @@ const StyledRoomButton = styled.button`
     background: rgba(255, 255, 255, 0.9);
     box-shadow: 0 6px 20px rgb(93 93 93 / 23%);
   }
+
+  /* Set colors based on the text content */
+  background-color: ${(props) =>
+    props.state === "Keine Angabe"
+      ? "grey"
+      : props.state === "Plätze frei"
+      ? "green"
+      : props.state === "Wenige Plätze frei"
+      ? "orange"
+      : "red"};
 `;
 
 export { StyledRoomButton };

fdc80d6c94772c395b49f058a8588640b53afab4
Ekaterina Bruch <ekaterinabruch@gmail.com>
2023-07-24 10:32:03 +0200

    Add comments

--- pages/api/rooms.js
+++ pages/api/rooms.js
@@ -3,10 +3,10 @@ import dbConnect from "../../db/models/connect";
 import Room from "../../db/models/Room";
 
 export default async function handler(request, response) {
-  await dbConnect();
+  await dbConnect(); //establishing connection with MongoDB database
 
   if (request.method === "GET") {
-    const rooms = await Room.find();
+    const rooms = await Room.find(); //find all rooms in the database
     response.status(200).json(rooms);
     return;
   }

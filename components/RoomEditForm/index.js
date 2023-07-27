905c4a3d3ea526445f869903163fee0b58be0294
Ekaterina Bruch <ekaterinabruch@gmail.com>
2023-07-27 21:09:08 +0200

    Function to update the room data

--- components/RoomEditForm/index.js
+++ components/RoomEditForm/index.js
@@ -1,24 +1,63 @@
+import { useState } from "react";
 import Heading from "../Heading";
 import { FormContainer, Label, Input, Form, SubmitButton } from "./styles";
+import { useRouter } from "next/router";
 
 export default function RoomEditForm({ room }) {
+  const router = useRouter();
+  const [formData, setFormData] = useState({
+    roomName: room ? room.name : "",
+    roomSubject: room ? room.subject : "",
+  });
+
+  async function handleSubmitForm(event) {
+    event.preventDefault();
+
+    const response = await fetch(`/api/rooms/${room._id}`, {
+      method: "PUT",
+      body: JSON.stringify({
+        roomName: formData.roomName,
+        roomSubject: formData.roomSubject,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (response.ok) {
+      console.log("Data successfully updated!");
+      router.push("/admin");
+    } else {
+      console.error("There seems to be a problem updating the data");
+    }
+  }
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: value,
+    }));
+  }
+
   return (
     <FormContainer>
       <Heading>Bearbeite den Raum</Heading>
-      <Form>
+      <Form onSubmit={handleSubmitForm}>
         <Label htmlFor="roomName">Raumbezeichnung</Label>
         <Input
           type="text"
           id="roomName"
           name="roomName"
-          defaultValue={room ? room.name : ""}
+          dvalue={formData.roomName}
+          onChange={handleChange}
         />
         <Label htmlFor="roomSubject">Fach</Label>
         <Input
           type="text"
           id="roomSubject"
           name="roomSubject"
-          defaultValue={room ? room.subject : ""}
+          value={formData.roomSubject}
+          onChange={handleChange}
         />
         <SubmitButton type="submit">Änderungen speichern</SubmitButton>
       </Form>

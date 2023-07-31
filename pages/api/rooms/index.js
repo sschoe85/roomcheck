5ed0bcfd85437f4e880e430698e6cb33922d4676
Ekaterina Bruch <ekaterinabruch@gmail.com>
2023-07-31 11:54:51 +0200

    Add backend logic and reset function

--- pages/api/rooms/index.js
+++ pages/api/rooms/index.js
@@ -1,6 +1,15 @@
 import dbConnect from "../../../db/models/connect";
 import Room from "../../../db/models/Room";
 
+async function resetRoomStates() {
+  try {
+    await Room.updateMany({}, { status: "Keine Angabe" });
+    console.log("Room states have been reset.");
+  } catch (error) {
+    console.error("Error resetting room states:", error);
+  }
+}
+
 export default async function handler(request, response) {
   console.log("API Request received:", request.method);
 
@@ -22,6 +31,17 @@ export default async function handler(request, response) {
     });
 
     response.status(200).json({ status: "Room created." });
+    return;
+  }
+
+  if (request.method === "PUT") {
+    const now = new Date();
+    if (now.getHours() === 10 && now.getMinutes() === 50) {
+      await resetRoomStates();
+      response.status(200).json({ status: "Room states have been reset." });
+    } else {
+      response.status(200).json({ status: "No action needed." });
+    }
     return;
   }
 

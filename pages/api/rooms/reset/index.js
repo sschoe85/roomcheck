d766e0757559a8d738cdc7446d8225c945f7430d
Ekaterina Bruch <ekaterinabruch@gmail.com>
2023-08-01 11:52:10 +0200

    Testing cronjob

--- pages/api/rooms/reset/index.js
+++ pages/api/rooms/reset/index.js
@@ -15,17 +15,9 @@ export default async function handler(request, response) {
   console.log("API Request received:", request.method);
 
   if (request.method === "PUT") {
-    const now = new Date();
-    if (
-      (now.getHours() === 11 && now.getMinutes() === 0) ||
-      (now.getHours() === 11 && now.getMinutes() === 20) ||
-      (now.getHours() === 23 && now.getMinutes() === 50)
-    ) {
-      await resetRoomStates();
-      response.status(200).json({ status: "Room states have been reset." });
-    } else {
-      response.status(200).json({ status: "No action needed." });
-    }
+    // Execute the resetRoomStates function when a PUT request is received
+    await resetRoomStates();
+    response.status(200).json({ status: "Room states have been reset." });
     return;
   }
 
